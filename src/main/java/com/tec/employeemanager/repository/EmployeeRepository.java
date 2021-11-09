1046184d92051bb070b156e239d207d54f7c3f21
mohamed khattab <mohamed.khattab84@yahoo.com>
2021-11-09 19:15:09 +0200

    adding repository package

---
+++ src/main/java/com/tec/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.tec.employeemanager.repository;
+
+import com.tec.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
