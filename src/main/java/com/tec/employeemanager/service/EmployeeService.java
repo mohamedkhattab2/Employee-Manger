9b874639f3e7567c062a1746db8da1ead2f0098c
mohamed khattab <mohamed.khattab84@yahoo.com>
2021-11-10 19:17:16 +0200

    add employee

--- src/main/java/com/tec/employeemanager/service/EmployeeService.java
+++ src/main/java/com/tec/employeemanager/service/EmployeeService.java
@@ -1,13 +1,25 @@
 package com.tec.employeemanager.service;
 
+import com.tec.employeemanager.model.Employee;
 import com.tec.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
     @Autowired
     public EmployeeService(EmployeeRepository employeeRepository){
-        this.employeeRepository = employeeRepository
+        this.employeeRepository = employeeRepository;
     }
+
+    // add Employee
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
 }
