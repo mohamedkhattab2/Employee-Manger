640147d32e57a4bbad902ef7d606d809ecb06f32
mohamed khattab <mohamed.khattab84@yahoo.com>
2021-11-10 19:37:45 +0200

    add all CRUD operation for service layer

--- src/main/java/com/tec/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/tec/employeemanager/repository/EmployeeRepository.java
@@ -3,6 +3,11 @@ package com.tec.employeemanager.repository;
 import com.tec.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/tec/employeemanager/service/EmployeeService.java
+++ src/main/java/com/tec/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.tec.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,5 +22,34 @@ public class EmployeeService {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
     }
+
+    // find all employees
+    public List<Employee> findAllEmployee(){
+        return employeeRepository.findAll();
+    }
+
+    // Update an employee
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    // find Employee by id
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findEmployeeById(id).orElse(
+                ()-> new UserNotFoundException("User by id " + id + "was not found"));
+
+    }
+
+    // Delete an employee
+   public void deleteEmployee(Long id){
+        employeeRepository.deleteEmployeeById(id);
+   }
+
+
+
+
+
+
+
 
 }
